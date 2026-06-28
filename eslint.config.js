05165e5879c712ff0ff3769ed8d47ea96db7db05
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-06-28 04:49:04 +0000

    refactor: using react for now

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-trailing-spaces": "warn",
+      "eol-last": ["warn", "always"],
+      semi: ["warn", "always"],
+      indent: ["warn", 2],
+      quotes: ["warn", "double", { avoidEscape: true }],
+      "object-curly-spacing": ["warn", "always"],
+    },
+  },
+]);
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#ECEEF8] ">
+      <p className="font-mono">Monospace</p>
+      <p className="font-sans">Sans Serif</p>
+      <p className="font-serif">Serif</p>
+    </div>
+  );
+}
+
+export default App;
