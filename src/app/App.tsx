dcbd67dab0706723ac42551a0bb143889fb5437d
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-07-02 09:36:59 +0000

    refactor: new workspace

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-mono">Hello World</h1>
+      <h1 className="font-sans">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./main.css";
-import App from "./App.tsx";
+
+import App from "./app/App";
+
+import "./shared/styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
