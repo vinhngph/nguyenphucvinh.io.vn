24d48f46ade192243d1acd6025f86d1cf6d1e8c4
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-07-03 04:36:28 +0000

    chore: add ink color

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1 className="font-mono">Hello World</h1>
-      <h1 className="font-sans">Hello World</h1>
-    </>
-  );
+  return <div className="relative text-ink"></div>;
 }
 
 export default App;
